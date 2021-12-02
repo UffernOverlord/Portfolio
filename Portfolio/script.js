face7532bf28b918ce48b8f2743d2c31b14509e0
Luka Eremeishvili <leremeishvili@mail.ru>
2021-12-02 21:21:43 +0400

    upd1

---
+++ Portfolio/script.js
@@ -0,0 +1,33 @@
+var all = document.getElementById('all');
+var app = document.getElementById('app');
+var card = document.getElementById('card');
+var web = document.getElementById('web');
+
+var appfilt = document.querySelectorAll('.filter-app');
+var cardfilt = document.querySelectorAll('.filter-card');
+var webfilt = document.querySelectorAll('.filter-web');
+
+
+console.log(appfilt);
+// all.onclick() = function(e){
+//     appfilt.style.display = "block";
+//     cardfilt.style.display = "block";
+//     webfilt.style.display = "block";
+// }
+
+// app.onclick() = function(e){
+//     appfilt.style.display = "block";
+//     cardfilt.style.display = "none";
+//     webfilt.style.display = "none";
+// }
+// card.onclick() = function(e){
+//     appfilt.style.display = "none";
+//     cardfilt.style.display = "block";
+//     webfilt.style.display = "none";
+// }
+// web.onclick() = function(e){
+//     appfilt.style.display = "none";
+//     cardfilt.style.display = "none";
+//     webfilt.style.display = "block";
+// }
+
